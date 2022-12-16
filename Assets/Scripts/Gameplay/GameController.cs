eab1b8bd4141240e0098ffeffbbfe7faacb7fea0
NewdayTurnaway <moba545@gmail.com>
2022-12-16 23:24:10 +0300

    Add EnemiesDestroyedCount

--- Assets/Scripts/Gameplay/GameController.cs
+++ Assets/Scripts/Gameplay/GameController.cs
@@ -57,7 +57,7 @@ namespace Gameplay
             _enemyForcesController = new(_playerController);
             AddController(_enemyForcesController);
 
-            _levelProgressController = new(levelProgressConfig, _playerController);
+            _levelProgressController = new(levelProgressConfig, _playerController, _enemyForcesController.EnemyViews);
             _levelProgressController.LevelComplete += LevelComplete;
             AddController(_levelProgressController);
         }
