8c05b4b779b9d790cc3c17bc99344e31e921e6e4
Rami <Dunadan2007@yandex.ru>
2023-02-21 20:54:41 +0300

    Review fixes

--- Assets/Scripts/Scriptables/Asteroid/AsteroidsSpawnConfig.cs
+++ Assets/Scripts/Scriptables/Asteroid/AsteroidsSpawnConfig.cs
@@ -6,8 +6,8 @@ namespace Scriptables.Asteroid
     [CreateAssetMenu(fileName = nameof(AsteroidsSpawnConfig), menuName = "Configs/Asteroid/" + nameof(AsteroidsSpawnConfig))]
     public class AsteroidsSpawnConfig : ScriptableObject
     {
-        [field: SerializeField] public int MaxAsteroidsInSpace;
-        [field: SerializeField] public float FastAsteroidSpawnDelay;
-        [field: SerializeField] public List<AsteroidConfig> AsteroidConfigs;
+        [field: SerializeField] public int MaxAsteroidsInSpace { get; private set; }
+        [field: SerializeField] public float FastAsteroidSpawnDelay { get; private set; }
+        [field: SerializeField] public List<AsteroidConfig> AsteroidConfigs { get; private set; }
     }
 }
